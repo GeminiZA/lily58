b7cfbaeb5d297bf967ad445f140ee5ce90bb43f6
Tristan Theron <tristantheron@gmail.com>
2025-07-22 09:30:50 +0200

    Switch to master_left

--- config.h
+++ config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-#define MASTER_RIGHT
+#define MASTER_LEFT
 
 // Underglow
 /*
